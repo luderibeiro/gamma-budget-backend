c26d66bbc834d509bab9427b9cebb73ec0a69e06
Lude Ribeiro <ludeyuri07@gmail.com>
2024-05-31 23:48:55 -0300

    Fix OAuth2Validator Class now it is possible to login.

--- projekt/core/validators.py
+++ projekt/core/validators.py
@@ -1,15 +1,17 @@
+from django.contrib.auth import authenticate
 from oauth2_provider.oauth2_validators import OAuth2Validator
 
 
 class GammaBudgetOAuth2Validator(OAuth2Validator):
-    def validate_user(self, user):
-        return True
+    def validate_user(self, username, password, client, request, *args, **kwargs):
+        """
+        Check username and password correspond to a valid and active User
+        """
+        user = None
+        if user is None:
+            user = authenticate(email=username, password=password)
+        if user is not None and user.is_active:
+            request.user = user
+            return True
 
-    def validate_bearer_token(self, token, scopes, request):
-        return True
-
-    def get_default_scopes(self, client_id, request, *args, **kwargs):
-        return ["read", "write", "delete"]
-
-    def get_available_scopes(self):
-        return ["read", "write", "delete", "trust"]
+        return False
