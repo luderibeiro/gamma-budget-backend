73987850a5ca15cec1afee95e021ac34077fff25
Paulo Gonçalves Lima <paulogoncalves436@gmail.com>
2024-06-02 21:01:49 -0300

    fix(user): corrige erro na criação de usuario

--- gamma_budget/core/models/user.py
+++ gamma_budget/core/models/user.py
@@ -19,6 +19,7 @@ class UserManager(BaseUserManager):
 
         Args:
         ----
+            username (str): The username of the user.
             email (str): The email address of the user.
             password (str): The password of the user.
             **extra_fields: Additional fields for the user model.
@@ -30,16 +31,17 @@ class UserManager(BaseUserManager):
         if not email:
             raise ValueError("The given email must be set")
         email = self.normalize_email(email)
-        user = self.model(email=email, username=username, **extra_fields)
+        user = self.model(username=username, email=email, **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
         return user
 
-    def create_user(self, email, password=None, **extra_fields):
+    def create_user(self, username, email, password=None, **extra_fields):
         """Creates and saves a regular user with the given email and password.
 
         Args:
         ----
+            username (str): The username of the user.
             email (str): The email address of the user.
             password (str, optional): The password of the user.
             **extra_fields: Additional fields for the user model.
@@ -50,13 +52,14 @@ class UserManager(BaseUserManager):
         """
         extra_fields.setdefault("is_staff", False)
         extra_fields.setdefault("is_superuser", False)
-        return self._create_user(email, password, **extra_fields)
+        return self._create_user(username, email, password, **extra_fields)
 
     def create_superuser(self, username, email, password, **extra_fields):
         """Creates and saves a superuser with the given email and password.
 
         Args:
         ----
+            username (str): The username of the superuser.
             email (str): The email address of the superuser.
             password (str): The password of the superuser.
             **extra_fields: Additional fields for the user model.
@@ -71,14 +74,13 @@ class UserManager(BaseUserManager):
         """
         extra_fields.setdefault("is_staff", True)
         extra_fields.setdefault("is_superuser", True)
-        extra_fields.setdefault("username", username)
 
         if extra_fields.get("is_staff") is not True:
             raise ValueError("Superuser must have is_staff=True.")
         if extra_fields.get("is_superuser") is not True:
             raise ValueError("Superuser must have is_superuser=True.")
 
-        return self._create_user(email, password, username=username, **extra_fields)
+        return self._create_user(username, email, password, **extra_fields)
 
 
 class User(AbstractUser):
@@ -119,4 +121,5 @@ class User(AbstractUser):
         return self.is_superuser
 
     USERNAME_FIELD = "email"
-    REQUIRED_FIELDS: ClassVar[list[str]] = []
+    REQUIRED_FIELDS: ClassVar[list[str]] = ["username", "email"]
+
