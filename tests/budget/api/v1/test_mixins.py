6f66f1dd137ce8ed4143d2d4103f3dcbc3930305
Paulo Gonçalves Lima <paulogoncalves436@gmail.com>
2024-06-27 20:05:16 -0300

    tests(mixins): adiciona testes ao mixins

---
+++ tests/budget/api/v1/test_mixins.py
@@ -0,0 +1,68 @@
+import unittest
+from unittest.mock import Mock
+from rest_framework import status
+from rest_framework.response import Response
+from budget.api.v1.mixins import (
+    ExecuteUseCaseOnGetMixin,
+    ExecuteUseCaseOnDestroyMixin,
+    ExecuteUseCaseOnUpdateMixin,
+    ExecuteUseCaseOnPutMixin,
+    ExecuteUseCaseOnCreateMixin,
+)
+import pytest
+from unittest.mock import Mock, patch
+from rest_framework import status
+from rest_framework.response import Response
+from budget.api_output import DjangoApiOutput
+
+
+@pytest.fixture
+def mock_request():
+    return Mock()
+
+
+@pytest.fixture
+def execute_use_case_on_get_mixin():
+    return ExecuteUseCaseOnGetMixin()
+
+
+def test_get_method(execute_use_case_on_get_mixin, mock_request):
+    with patch.object(
+        execute_use_case_on_get_mixin,
+        "execute_use_case_retrieve",
+        return_value=Mock(get_response=Mock(return_value=Response(data={"test": "data"}, status=status.HTTP_200_OK))),
+    ):
+        response = execute_use_case_on_get_mixin.get(mock_request)
+        assert response.status_code == status.HTTP_200_OK
+        assert response.data == {"test": "data"}
+
+    # Test exception handling (example: HTTP 400 error)
+    with patch.object(
+        execute_use_case_on_get_mixin, "execute_use_case_retrieve", side_effect=execute_use_case_on_get_mixin.Http400Error("Bad request")
+    ):
+        response = execute_use_case_on_get_mixin.get(mock_request)
+        assert response.status_code == status.HTTP_400_BAD_REQUEST
+
+
+@pytest.fixture
+def execute_use_case_on_destroy_mixin():
+    return ExecuteUseCaseOnDestroyMixin()
+
+
+def test_delete_method(execute_use_case_on_destroy_mixin, mock_request):
+    with patch.object(
+        execute_use_case_on_destroy_mixin,
+        "execute_use_case_destroy",
+        return_value=Mock(get_response=Mock(return_value=Response(data={"test": "data"}, status=status.HTTP_200_OK))),
+    ):
+        response = execute_use_case_on_destroy_mixin.delete(mock_request)
+        assert response.status_code == status.HTTP_200_OK
+        assert response.data == {"test": "data"}
+
+    # Test exception handling (example: HTTP 400 error)
+    with pytest.raises(ExecuteUseCaseOnGetMixin.Http400Error), patch.object(
+        execute_use_case_on_destroy_mixin, "execute_use_case_destroy", side_effect=ExecuteUseCaseOnGetMixin.Http400Error("Bad request")
+    ):
+        response = execute_use_case_on_destroy_mixin.delete(mock_request)
+        assert response.status_code == status.HTTP_400_BAD_REQUEST
+
